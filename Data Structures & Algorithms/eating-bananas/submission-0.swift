c6a4f541ced5959f7df7640ac16b8ca1d1ca743d
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2026-09-15 22:46:42 -0400

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+        var low = 1
+        var high = piles.max()!
+
+        var rate = 0
+        while low < high {
+            rate = low + (high - low) / 2
+            let time = minEat(rate, piles)
+            if time > h {
+                low = rate + 1
+            } else {
+                high = rate
+            }
+        }
+        return low
+    }
+
+    func minEat(_ rate: Int, _ piles: [Int]) -> Int {
+        var count = 0
+        for pile in piles {
+            count += (rate + pile - 1)/rate
+        }
+
+        return count
+    }
+}

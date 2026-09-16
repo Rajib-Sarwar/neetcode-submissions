ab926dd254786d43e10e3c55bce87d02fc5bef82
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2026-09-15 23:20:44 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func findMin(_ nums: [Int]) -> Int {
+        var left = 0
+        var right = nums.count - 1
+
+        while left < right {
+            let mid = left + (right - left)/2
+            print("left: \(left), right: \(right) = mid \(mid)")
+            if nums[mid] > nums[nums.count - 1] {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+
+        return nums[right]
+    }
+} 

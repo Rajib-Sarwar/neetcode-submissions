5bcb154312f931300db8398ea1155c906d38f3a9
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2026-09-17 14:14:46 -0400

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.swift
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+
+        if root.left == nil && root.right == nil { return 1 }
+
+        let leftHeight = maxDepth(root.left)
+        let rightHeight = maxDepth(root.right)
+
+        return max(leftHeight, rightHeight) + 1
+    }
+}
